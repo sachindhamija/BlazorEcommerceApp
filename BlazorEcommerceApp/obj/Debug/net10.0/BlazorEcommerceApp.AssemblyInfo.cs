b67885a6031b3b08627695233b39763f037aa0de
sachindhamija <sdhamija728@gmail.com>
2025-07-17 01:36:32 -0400

    Project Setup

--- BlazorEcommerceApp/obj/Debug/net10.0/BlazorEcommerceApp.AssemblyInfo.cs
+++ BlazorEcommerceApp/obj/Debug/net10.0/BlazorEcommerceApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorEcommerceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e74d4d55319d94a1a97996a237bd850f57e7464f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorEcommerceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorEcommerceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
